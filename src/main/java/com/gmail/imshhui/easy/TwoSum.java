bea807102ae4db90a7012dbfd8d4ccd7649198ca
imshhui <imshhui@gmail.com>
2018-07-24 17:42:14 +0800

    fix border conditions bug

--- src/main/java/com/gmail/imshhui/easy/TwoSum.java
+++ src/main/java/com/gmail/imshhui/easy/TwoSum.java
@@ -14,10 +14,11 @@ package com.gmail.imshhui.easy;
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        int[] result = null;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
+                    result = new int[2];
                     result[0] = i;
                     result[1] = j;
                     break;
