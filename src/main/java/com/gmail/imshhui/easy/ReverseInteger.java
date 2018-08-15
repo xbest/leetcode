c7048b931d2c37cfc95b83d16ea19339324e96a2
imshhui <imshhui@gmail.com>
2018-08-15 14:50:35 +0800

    reverse integer comments

--- src/main/java/com/gmail/imshhui/easy/ReverseInteger.java
+++ src/main/java/com/gmail/imshhui/easy/ReverseInteger.java
@@ -1,6 +1,22 @@
 package com.gmail.imshhui.easy;
 
 /**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 123
+ * Output: 321
+ * Example 2:
+ * <p>
+ * Input: -123
+ * Output: -321
+ * Example 3:
+ * <p>
+ * Input: 120
+ * Output: 21
+ * Note:
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  * User: liyulin
  * Date: 2018/8/14
  */
--- src/main/java/com/gmail/imshhui/medium/ZigZagConversion.java
+++ src/main/java/com/gmail/imshhui/medium/ZigZagConversion.java
@@ -1,7 +1,5 @@
 package com.gmail.imshhui.medium;
 
-import java.nio.charset.Charset;
-
 /**
  * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
  * (you may want to display this pattern in a fixed font for better legibility)
