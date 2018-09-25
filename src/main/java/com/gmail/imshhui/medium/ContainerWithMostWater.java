0b1c758aed39f69f382f8c4fd3f06a931d9f1caa
imshhui <imshhui@gmail.com>
2018-09-25 16:21:46 +0800

    two pointer approach solution for container with most water

--- src/main/java/com/gmail/imshhui/medium/ContainerWithMostWater.java
+++ src/main/java/com/gmail/imshhui/medium/ContainerWithMostWater.java
@@ -25,4 +25,27 @@ public class ContainerWithMostWater {
         }
         return maxArea;
     }
+
+    /**
+     * two pointer approach
+     * theory reference by leetcode
+     * code by myself
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea2(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
 }
--- src/test/java/com/gmail/imshhui/medium/ContainerWithMostWaterTest.java
+++ src/test/java/com/gmail/imshhui/medium/ContainerWithMostWaterTest.java
@@ -14,5 +14,6 @@ public class ContainerWithMostWaterTest {
     public void maxAreaTest() {
         int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         Assert.assertEquals(49, client.maxArea(height));
+        Assert.assertEquals(49, client.maxArea2(height));
     }
 }
