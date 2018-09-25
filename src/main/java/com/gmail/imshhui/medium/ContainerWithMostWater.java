78450e169a2908f0b7ccbdfffc3cc6cb40024372
imshhui <imshhui@gmail.com>
2018-09-25 16:02:46 +0800

    brute force solution for container with most water

---
+++ src/main/java/com/gmail/imshhui/medium/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * <p>
+ * Note: You may not slant the container and n is at least 2.
+ * User: liyulin
+ * Date: 2018/9/25
+ */
+public class ContainerWithMostWater {
+    /**
+     * brute force solution
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
+            }
+        }
+        return maxArea;
+    }
+}
---
+++ src/test/java/com/gmail/imshhui/medium/ContainerWithMostWaterTest.java
@@ -0,0 +1,18 @@
+package com.gmail.imshhui.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * User: liyulin
+ * Date: 2018/9/25
+ */
+public class ContainerWithMostWaterTest {
+    private ContainerWithMostWater client = new ContainerWithMostWater();
+
+    @Test
+    public void maxAreaTest() {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Assert.assertEquals(49, client.maxArea(height));
+    }
+}
