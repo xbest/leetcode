2d29bddd5b27d9595b237e7a41fc0afe3e71c035
imshhui <imshhui@gmail.com>
2019-07-09 17:39:55 +0800

    stack solution for valid parentheses

---
+++ src/main/java/com/gmail/imshhui/easy/ValidParentheses.java
@@ -0,0 +1,55 @@
+package com.gmail.imshhui.easy;
+
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ * <p>
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ * <p>
+ * Input: "{[]}"
+ * Output: true
+ * User: liyulin
+ * Date: 2019/7/9
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
