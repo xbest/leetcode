ebd69b7fcfc27a0009bb38328afc0abb71937f48
imshhui <imshhui@gmail.com>
2019-07-11 11:03:44 +0800

    recursive solution for swap nodes in pairs

---
+++ src/main/java/com/gmail/imshhui/medium/SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+package com.gmail.imshhui.medium;
+
+import com.gmail.imshhui.bean.ListNode;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p>
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ * <p>
+ * Example:
+ * <p>
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *
+ * @see <a href="https://leetcode.com/problems/swap-nodes-in-pairs/"></a>
+ * User: liyulin
+ * Date: 2019/7/10
+ */
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
