3a823d52f66d3e44b6a543c8a3a24305da9581aa
imshhui <imshhui@gmail.com>
2019-07-11 11:31:58 +0800

    iterative solution for swap nodes in pairs

--- src/main/java/com/gmail/imshhui/medium/SwapNodesInPairs.java
+++ src/main/java/com/gmail/imshhui/medium/SwapNodesInPairs.java
@@ -25,4 +25,19 @@ public class SwapNodesInPairs {
         n.next = head;
         return n;
     }
+
+    public ListNode swapPairsIteration(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            current.next = second;
+            first.next = second.next;
+            second.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
 }
