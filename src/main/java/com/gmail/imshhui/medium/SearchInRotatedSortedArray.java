b5e5921a347d332394861f1d7228a4dbabad99d7
imshhui <imshhui@gmail.com>
2019-07-17 17:11:41 +0800

    binary search solution for search in rotated sorted array

---
+++ src/main/java/com/gmail/imshhui/medium/SearchInRotatedSortedArray.java
@@ -0,0 +1,53 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * <p>
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * <p>
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * <p>
+ * You may assume no duplicate exists in the array.
+ * <p>
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ * <p>
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ *
+ * @see <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/"></a>
+ * User: liyulin
+ * Date: 2019/7/17
+ */
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // the left side is ascend
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
