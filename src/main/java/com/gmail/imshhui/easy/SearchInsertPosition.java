eeb9321ce1e378e991d34fa068bcb6407e068858
imshhui <imshhui@gmail.com>
2019-07-18 14:52:37 +0800

    solution for search insert position

---
+++ src/main/java/com/gmail/imshhui/easy/SearchInsertPosition.java
@@ -0,0 +1,38 @@
+package com.gmail.imshhui.easy;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * Example 4:
+ * <p>
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ *
+ * @see <a href="https://leetcode.com/problems/search-insert-position/"></a>
+ * User: liyulin
+ * Date: 2019/7/18
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
