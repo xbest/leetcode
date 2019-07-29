e9147be143d98f998d272623574bccf3efe6c186
imshhui <imshhui@gmail.com>
2019-07-29 15:10:34 +0800

    fibonacci solution for climbing stairs

--- src/main/java/com/gmail/imshhui/easy/ClimbingStairs.java
+++ src/main/java/com/gmail/imshhui/easy/ClimbingStairs.java
@@ -66,4 +66,17 @@ public class ClimbingStairs {
         return n;
     }
 
+    public int climbStairsDp(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] ret = new int[n];
+        ret[0] = 1;
+        ret[1] = 2;
+        for (int i = 2; i < n; i++) {
+            ret[i] = ret[i - 1] + ret[i - 2];
+        }
+        return ret[n - 1];
+    }
+
 }
--- src/test/java/com/gmail/imshhui/easy/ClimbingStairsTest.java
+++ src/test/java/com/gmail/imshhui/easy/ClimbingStairsTest.java
@@ -12,11 +12,12 @@ public class ClimbingStairsTest {
 
     @Test
     public void climbStairs() {
-        Assert.assertEquals(2, client.climbStairs(2));
-        Assert.assertEquals(3, client.climbStairs(3));
-        Assert.assertEquals(1134903170, client.climbStairs(44));
-        Assert.assertEquals(2, client.climbStairs1(2));
-        Assert.assertEquals(3, client.climbStairs1(3));
-        Assert.assertEquals(1134903170, client.climbStairs1(44));
+//        Assert.assertEquals(2, client.climbStairs(2));
+//        Assert.assertEquals(3, client.climbStairs(3));
+//        Assert.assertEquals(1134903170, client.climbStairs(44));
+//        Assert.assertEquals(2, client.climbStairs1(2));
+//        Assert.assertEquals(3, client.climbStairs1(3));
+//        Assert.assertEquals(1134903170, client.climbStairs1(44));
+        Assert.assertEquals(1134903170, client.climbStairsDp(44));
     }
 }
