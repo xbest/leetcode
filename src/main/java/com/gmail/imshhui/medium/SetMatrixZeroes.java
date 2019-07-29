7aa3106cef4bc9c7b5e2f57c80495f615091367c
imshhui <imshhui@gmail.com>
2019-07-29 17:20:30 +0800

    O(m+n) space solution for set matrix zeroes

---
+++ src/main/java/com/gmail/imshhui/medium/SetMatrixZeroes.java
@@ -0,0 +1,66 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * Output:
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * Example 2:
+ * <p>
+ * Input:
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ * Follow up:
+ * <p>
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
+ *
+ * @see <a href="https://leetcode.com/problems/set-matrix-zeroes/"></a>
+ * User: liyulin
+ * Date: 2019/7/29
+ */
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] columns = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    columns[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || columns[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
