b5214fc2aad3721d52cce8ef5b021a6a165d2ab9
imshhui <imshhui@gmail.com>
2019-07-30 09:18:57 +0800

    fix empty matrix case

--- src/main/java/com/gmail/imshhui/medium/SearchA2DMatrix.java
+++ src/main/java/com/gmail/imshhui/medium/SearchA2DMatrix.java
@@ -32,6 +32,9 @@ package com.gmail.imshhui.medium;
  */
 public class SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
         int rows = matrix.length;
         int cols = matrix[0].length;
         int start = 0;
