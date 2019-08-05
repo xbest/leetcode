9682aa0a16dd0e5b47e908942dc6c20fc086db7c
imshhui <imshhui@gmail.com>
2019-08-05 14:46:03 +0800

    two pointers solution for remove duplicates from sorted list II

---
+++ src/main/java/com/gmail/imshhui/medium/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,43 @@
+package com.gmail.imshhui.medium;
+
+import com.gmail.imshhui.bean.ListNode;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * Example 2:
+ * <p>
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ *
+ * @see <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/"></a>
+ * User: liyulin
+ * Date: 2019/8/5
+ */
+public class RemoveDuplicatesFromSortedListII {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode pre = dummy;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
+            }
+            if (pre.next == cur) {
+                pre = pre.next;
+            } else {
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
