21b44caab5e7c6af4717161ab7eb6f920de78151
imshhui <imshhui@gmail.com>
2019-08-16 16:25:15 +0800

    recursive solution for same tree

--- src/main/java/com/gmail/imshhui/easy/SameTree.java
+++ src/main/java/com/gmail/imshhui/easy/SameTree.java
@@ -65,4 +65,14 @@ public class SameTree {
         }
         return p == null && q == null;
     }
+
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree1(p.left, q.left) && isSameTree1(p.right, q.right);
+    }
 }
