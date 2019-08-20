5b74867ec1204aecd6630b32f333c422dac447d2
imshhui <imshhui@gmail.com>
2019-08-20 14:44:33 +0800

    solution for construct binary tree from preorder and inorder traversal with map and list

--- src/main/java/com/gmail/imshhui/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/com/gmail/imshhui/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -2,6 +2,12 @@ package com.gmail.imshhui.medium;
 
 import com.gmail.imshhui.bean.TreeNode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Given preorder and inorder traversal of a tree, construct the binary tree.
  *
@@ -43,7 +49,29 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         }
         root.left = rootHelper(preStart + 1, inStart, i - 1, preorder, inorder);
         // confused why i - inStart?
+        // Because there are i-inStart nodes left in the left tree, the left nodes will use the preorder array with i-inStart?
         root.right = rootHelper(preStart + i - inStart + 1, i + 1, inEnd, preorder, inorder);
+        return root;
+    }
+
+    public TreeNode buildTree1(int[] preoder, int[] inorder) {
+        List<Integer> preorderList = Arrays.stream(preoder).boxed().collect(Collectors.toList());
+        Map<Integer, Integer> inorderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return rootHelperMap(preorderList, inorderMap, 0, inorder.length - 1);
+    }
+
+    private TreeNode rootHelperMap(List<Integer> preorderList, Map<Integer, Integer> inorderMap, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+        int value = preorderList.remove(0);
+        TreeNode root = new TreeNode(value);
+        int i = inorderMap.get(value);
+        root.left = rootHelperMap(preorderList, inorderMap, inStart, i - 1);
+        root.right = rootHelperMap(preorderList, inorderMap, i + 1, inEnd);
         return root;
     }
 }
