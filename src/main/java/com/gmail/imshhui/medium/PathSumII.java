5b0312e25629fa787403c30abe512001053dbb5d
imshhui <imshhui@gmail.com>
2019-08-21 17:32:11 +0800

    another DFS solution without return when matched for path sum II

--- src/main/java/com/gmail/imshhui/medium/PathSumII.java
+++ src/main/java/com/gmail/imshhui/medium/PathSumII.java
@@ -55,4 +55,19 @@ public class PathSumII {
         pathHelper(list, temp, root.right, path, sum);
         temp.remove(temp.size() - 1);
     }
+
+    private void pathHelper2(List<List<Integer>> list, List<Integer> temp, TreeNode root, int path, int sum) {
+        if (root == null) {
+            return;
+        }
+        path = path + root.val;
+        temp.add(root.val);
+        if (path == sum && root.left == null && root.right == null) {
+            list.add(new ArrayList<>(temp));
+        } else {
+            pathHelper(list, temp, root.left, path, sum);
+            pathHelper(list, temp, root.right, path, sum);
+        }
+        temp.remove(temp.size() - 1);
+    }
 }
