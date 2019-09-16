1ee7672191a569c05f713f18831fd4a8d510141e
imshhui <imshhui@gmail.com>
2019-09-16 15:22:20 +0800

    brute force solution for single number

---
+++ src/main/java/com/gmail/imshhui/easy/SingleNumber.java
@@ -0,0 +1,35 @@
+package com.gmail.imshhui.easy;
+
+import java.util.Arrays;
+
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ * @see <a href="https://leetcode.com/problems/single-number/"></a>
+ * User: liyulin
+ * Date: 2019/9/16
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i = i + 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
