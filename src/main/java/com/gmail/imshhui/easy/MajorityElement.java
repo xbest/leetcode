2905fe0650c99f764f0c8339863759bce1e3f4bf
imshhui <imshhui@gmail.com>
2019-09-23 23:24:21 +0800

    more votes solution for majority element

--- src/main/java/com/gmail/imshhui/easy/MajorityElement.java
+++ src/main/java/com/gmail/imshhui/easy/MajorityElement.java
@@ -25,4 +25,20 @@ public class MajorityElement {
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }
+
+    public int majorityElement1(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                major = nums[i];
+                count++;
+            } else if (major == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return major;
+    }
 }
