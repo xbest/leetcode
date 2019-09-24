04659c67b59ba42414e8e7ce62fbdcdf392fddc0
imshhui <imshhui@gmail.com>
2019-09-24 09:24:09 +0800

    another solution for excel sheet column number

--- src/main/java/com/gmail/imshhui/easy/ExcelSheetColumnNumber.java
+++ src/main/java/com/gmail/imshhui/easy/ExcelSheetColumnNumber.java
@@ -39,4 +39,12 @@ public class ExcelSheetColumnNumber {
         }
         return res;
     }
+
+    public int titleToNumber1(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res = res * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return res;
+    }
 }
