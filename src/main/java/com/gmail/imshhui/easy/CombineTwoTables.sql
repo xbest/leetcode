2da05e5a4006e72f2fba2e83f2a07dc867d510cb
imshhui <imshhui@gmail.com>
2019-09-24 11:40:07 +0800

    SQL for second highest salary

---
+++ src/main/java/com/gmail/imshhui/easy/CombineTwoTables.sql
@@ -0,0 +1,42 @@
+/** SQL Schema **/
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York');
+
+/*************** Problem Description ***************
+
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+
+
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+FirstName, LastName, City, State
+
+*************** Problem Description ***************/
+
+/*************** Problem Solution ***************/
+select FirstName, LastName, City, State from Person p left join Address a on p.PersonId = a.PersonId;
+/*************** Problem Solution ***************/
---
+++ src/main/java/com/gmail/imshhui/easy/SecondHighestSalary.sql
@@ -0,0 +1,30 @@
+/** SQL Schema **/
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
+/*************** Problem Description ***************
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*************** Problem Description ***************/
+
+/*************** Problem Solution ***************/
+select max(Salary) as SecondHighestSalary from Employee where Salary < (select max(Salary)from Employee);
+select (select distinct Salary from Employee order by Salary desc limit 1 offset 1) as SecondHighestSalary;
+/*************** Problem Solution ***************/
