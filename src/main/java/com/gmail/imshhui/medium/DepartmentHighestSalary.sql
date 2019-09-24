74bee6b6d201935d79a776f94602bdd2a8f47ed0
imshhui <imshhui@gmail.com>
2019-09-24 17:51:27 +0800

    SQL for department highest salary

---
+++ src/main/java/com/gmail/imshhui/medium/DepartmentHighestSalary.sql
@@ -0,0 +1,71 @@
+/*************** SQL Schema ***************/
+Create table If Not Exists Employee
+(
+  Id           int,
+  Name         varchar(255),
+  Salary       int,
+  DepartmentId int
+);
+Create table If Not Exists Department
+(
+  Id   int,
+  Name varchar(255)
+);
+Truncate table Employee;
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('1', 'Joe', '70000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('2', 'Jim', '90000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('3', 'Henry', '80000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('4', 'Sam', '60000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('5', 'Max', '90000', '1');
+Truncate table Department;
+insert into Department (Id, Name)
+values ('1', 'IT');
+insert into Department (Id, Name)
+values ('2', 'Sales');
+
+/*************** Problem Description ***************
+https://leetcode.com/problems/department-highest-salary/
+
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, your SQL query should return the following rows (order of rows does not matter).
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+Explanation:
+
+Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+ *************** Problem Description ***************/
+
+/*************** Problem Solution ***************/
+select Department.Name as Department, Employee.Name as Employee, Salary
+from Employee left join Department on DepartmentId = Department.Id
+where (DepartmentId, Salary) in (select DepartmentId, max(Salary) from Employee group by DepartmentId);
+/*************** Problem Solution ***************/
