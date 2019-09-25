024652985ead748ecca9ef398f6be818e7d87c49
imshhui <imshhui@gmail.com>
2019-09-25 15:05:44 +0800

    SQL for rising temperature

---
+++ src/main/java/com/gmail/imshhui/easy/RisingTemperature.sql
@@ -0,0 +1,34 @@
+/*************** SQL Schema ***************/
+Create table If Not Exists Weather (Id int, RecordDate date, Temperature int);
+Truncate table Weather;
+insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10');
+insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25');
+insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20');
+insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30');
+
+/*************** Problem Description ***************
+https://leetcode.com/problems/rising-temperature/
+
+Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+ *************** Problem Description ***************/
+
+/*************** Problem Solution ***************/
+select w1.Id as Id from Weather w1, Weather w2 where datediff(w1.RecordDate,w2.RecordDate)=1 and w1.Temperature>w2.Temperature;
+/*************** Problem Solution ***************/
