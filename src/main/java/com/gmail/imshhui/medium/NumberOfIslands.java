bbc41b14f91fc0d7559f2d6677db8c63d449da5c
imshhui <imshhui@gmail.com>
2019-09-25 19:18:01 +0800

    DFS solution for number of islands

---
+++ src/main/java/com/gmail/imshhui/medium/NumberOfIslands.java
@@ -0,0 +1,52 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * Output: 3
+ *
+ * @see <a href="https://leetcode.com/problems/number-of-islands/"></a>
+ * User: liyulin
+ * Date: 2019/9/25
+ */
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+        }
+    }
+}
