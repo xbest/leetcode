67245f09ed1098e0c7c8eb611e3f061d1a385ff3
imshhui <imshhui@gmail.com>
2019-09-30 15:28:30 +0800

    Implement Trie (Prefix Tree)

--- src/main/java/com/gmail/imshhui/medium/Trie.java
+++ src/main/java/com/gmail/imshhui/medium/Trie.java
@@ -23,25 +23,84 @@ package com.gmail.imshhui.medium;
  * Date: 2019/9/30
  */
 class Trie {
+    private TrieNode root;
 
     /** Initialize your data structure here. */
     public Trie() {
-
+        root = new TrieNode();
     }
 
     /** Inserts a word into the trie. */
     public void insert(String word) {
-
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            // bad code
+//            if (node.containsKey(c)) {
+//                node = node.get(c);
+//            } else {
+//                node.put(c, new TrieNode());
+//            }
+            if (!node.containsKey(c)) {
+                node.put(c, new TrieNode());
+            }
+            node = node.get(c);
+        }
+        node.setEnd();
     }
 
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
 
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length() && node != null; i++) {
+            char c = prefix.charAt(i);
+            if (node.containsKey(c)) {
+                node = node.get(c);
+            } else {
+                node = null;
+            }
+        }
+        return node;
+    }
+
+    private class TrieNode {
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char c) {
+            return links[c - 'a'] != null;
+        }
+
+        public TrieNode get(char c) {
+            return links[c - 'a'];
+        }
+
+        public void put(char c, TrieNode node) {
+            links[c - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
     }
 }
 
