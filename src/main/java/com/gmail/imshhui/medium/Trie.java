02dc1930cab307e4045eb419c9387af0358f1255
imshhui <imshhui@gmail.com>
2019-09-30 15:27:31 +0800

    Implement Trie (Prefix Tree)

---
+++ src/main/java/com/gmail/imshhui/medium/Trie.java
@@ -0,0 +1,54 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Implement a trie with insert, search, and startsWith methods.
+ *
+ * Example:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");
+ * trie.search("app");     // returns true
+ * Note:
+ *
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
+ *
+ * @see <a href="https://leetcode.com/problems/implement-trie-prefix-tree/"></a>
+ * User: liyulin
+ * Date: 2019/9/30
+ */
+class Trie {
+
+    /** Initialize your data structure here. */
+    public Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
