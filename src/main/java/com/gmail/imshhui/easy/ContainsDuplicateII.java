0a12b4b386605463e39ca39a313ac8d93301c0a6
imshhui <imshhui@gmail.com>
2019-10-09 11:50:19 +0800

    HashMap solution for contain duplicate II

---
+++ src/main/java/com/gmail/imshhui/easy/ContainsDuplicateII.java
@@ -0,0 +1,39 @@
+package com.gmail.imshhui.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers and an integer k,
+ * find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * Example 2:
+ *
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * Example 3:
+ *
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ *
+ * @see <a href="https://leetcode.com/problems/contains-duplicate-ii/"></a>
+ * User: liyulin
+ * Date: 2019/10/9
+ */
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
