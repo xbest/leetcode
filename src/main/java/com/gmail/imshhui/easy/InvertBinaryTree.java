e1fb08a5fb77b68d417c0e10698f5735ce100314
imshhui <imshhui@gmail.com>
2019-10-10 10:23:14 +0800

    recursive solution for invert binary tree

---
+++ src/main/java/com/gmail/imshhui/easy/InvertBinaryTree.java
@@ -0,0 +1,46 @@
+package com.gmail.imshhui.easy;
+
+import com.gmail.imshhui.bean.TreeNode;
+
+/**
+ * Invert a binary tree.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * Output:
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ *
+ * Google: 90% of our engineers use the software you wrote (Homebrew),
+ * but you can’t invert a binary tree on a whiteboard so f*** off.
+ *
+ * @see <a href="https://leetcode.com/problems/invert-binary-tree/"></a>
+ * User: liyulin
+ * Date: 2019/10/10
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
