c4c348b7a29ce608e819d5ac498beb025afabbd7
imshhui <imshhui@gmail.com>
2019-10-12 15:05:14 +0800

    one array and constant space complexity for product of array except self

--- src/main/java/com/gmail/imshhui/medium/ProductOfArrayExceptSelf.java
+++ src/main/java/com/gmail/imshhui/medium/ProductOfArrayExceptSelf.java
@@ -36,4 +36,19 @@ public class ProductOfArrayExceptSelf {
         }
         return output;
     }
+
+    public int[] productExceptSelfOneArray(int[] nums) {
+        int length = nums.length;
+        int[] ans = new int[length];
+        ans[0] = 1;
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = right * ans[i];
+            right = right * nums[i];
+        }
+        return ans;
+    }
 }
