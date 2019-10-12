6874e7fbb813106a01eadd28059d7b6489426fe2
imshhui <imshhui@gmail.com>
2019-10-12 10:07:56 +0800

    Boyer-Moore solution for majority element II

--- src/main/java/com/gmail/imshhui/medium/MajorityElementII.java
+++ src/main/java/com/gmail/imshhui/medium/MajorityElementII.java
@@ -1,5 +1,8 @@
 package com.gmail.imshhui.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
  *
@@ -19,7 +22,46 @@ package com.gmail.imshhui.medium;
  * Date: 2019/10/10
  */
 public class MajorityElementII {
-//    public List<Integer> majorityElement(int[] nums) {
-//        int count = nums.length/3;
-//    }
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        int n1 = nums[0];
+        int n2 = 0;
+        int c1 = 1;
+        int c2 = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == n1) {
+                c1++;
+            } else if (nums[i] == n2) {
+                c2++;
+            } else if (c1 == 0) {
+                n1 = nums[i];
+                c1 = 1;
+            } else if (c2 == 0) {
+                n2 = nums[i];
+                c2 = 1;
+            } else {
+                c1--;
+                c2--;
+            }
+        }
+        c1 = 0;
+        c2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == n1) {
+                c1++;
+            } else if (nums[i] == n2) {
+                c2++;
+            }
+        }
+        if (c1 > nums.length / 3) {
+            res.add(n1);
+        }
+        if (c2 > nums.length / 3) {
+            res.add(n2);
+        }
+        return res;
+    }
 }
