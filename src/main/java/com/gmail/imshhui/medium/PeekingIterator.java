55c7e192bb28c5f278b1eb2a0288e8b6412d14ed
imshhui <imshhui@gmail.com>
2019-10-15 11:48:05 +0800

    better space complexity solution for peeking iterator

--- src/main/java/com/gmail/imshhui/medium/PeekingIterator.java
+++ src/main/java/com/gmail/imshhui/medium/PeekingIterator.java
@@ -1,8 +1,6 @@
 package com.gmail.imshhui.medium;
 
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * Given an Iterator class interface with methods: next() and hasNext(),
@@ -25,23 +23,20 @@ import java.util.Queue;
  */
 public class PeekingIterator implements Iterator<Integer> {
     private Iterator<Integer> iterator;
-    private Queue<Integer> list;
+    private Integer next;
 
     public PeekingIterator(Iterator<Integer> iterator) {
         // initialize any member here.
         this.iterator = iterator;
-        list = new LinkedList<>();
     }
 
     // Returns the next element in the iteration without advancing the iterator.
     public Integer peek() {
-        Integer next = list.peek();
         if (next != null) {
             return next;
         }
         if (hasNext()) {
             next = iterator.next();
-            list.add(next);
         }
         return next;
     }
@@ -50,12 +45,18 @@ public class PeekingIterator implements Iterator<Integer> {
     // Override them if needed.
     @Override
     public Integer next() {
-        Integer next = list.poll();
-        return next == null && iterator.hasNext() ? iterator.next() : next;
+        Integer temp;
+        if (next != null) {
+            temp = next;
+            next = null;
+        } else {
+            temp = iterator.next();
+        }
+        return temp;
     }
 
     @Override
     public boolean hasNext() {
-        return !list.isEmpty() || iterator.hasNext();
+        return next != null || iterator.hasNext();
     }
 }
