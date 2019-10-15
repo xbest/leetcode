df98dfd344cb0426b6aba9fc92824d401e6b1d05
imshhui <imshhui@gmail.com>
2019-10-15 14:47:47 +0800

    two pointers solution which is same as Linked List Cycle II for find the duplicate number

--- src/main/java/com/gmail/imshhui/medium/FindTheDuplicateNumber.java
+++ src/main/java/com/gmail/imshhui/medium/FindTheDuplicateNumber.java
@@ -38,4 +38,22 @@ public class FindTheDuplicateNumber {
         }
         return -1;
     }
+
+    public int findDuplicateTwoPointers(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
 }
