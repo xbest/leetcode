b39b0327bdc83b7836510a5d932a46e1b160fbfc
imshhui <imshhui@gmail.com>
2019-10-16 17:20:11 +0800

    Naive solution for Range Sum Query - Mutable

---
+++ src/main/java/com/gmail/imshhui/medium/NumArray.java
@@ -0,0 +1,48 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ *
+ * The update(i, val) function modifies nums by updating the element at index i to val.
+ *
+ * Example:
+ *
+ * Given nums = [1, 3, 5]
+ *
+ * sumRange(0, 2) -> 9
+ * update(1, 2)
+ * sumRange(0, 2) -> 8
+ * Note:
+ *
+ * The array is only modifiable by the update function.
+ * You may assume the number of calls to update and sumRange function is distributed evenly.
+ *
+ * @see <a href="https://leetcode.com/problems/range-sum-query-mutable/">307. Range Sum Query - Mutable</a>
+ * User: liyulin
+ * Date: 2019/10/16
+ */
+public class NumArray {
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public void update(int i, int val) {
+        nums[i] = val;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for (int k = i; k <= j; k++) {
+            sum = sum + nums[k];
+        }
+        return sum;
+    }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
