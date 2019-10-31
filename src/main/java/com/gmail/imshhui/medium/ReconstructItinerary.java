6157296585c49dba6129bd12808148828dd02ce7
imshhui <imshhui@gmail.com>
2019-10-31 11:35:14 +0800

    Eulerian path and Hierholzer's algorithm for Reconstruct Itinerary

---
+++ src/main/java/com/gmail/imshhui/medium/ReconstructItinerary.java
@@ -0,0 +1,50 @@
+package com.gmail.imshhui.medium;
+
+import java.util.*;
+
+/**
+ * Given a list of airline tickets represented by pairs of departure and arrival airports [from, to],
+ * reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK.
+ * Thus, the itinerary must begin with JFK.
+ *
+ * Note:
+ *
+ * If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+ * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ * All airports are represented by three capital letters (IATA code).
+ * You may assume all tickets form at least one valid itinerary.
+ * Example 1:
+ *
+ * Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+ * Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+ * Example 2:
+ *
+ * Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+ *              But it is larger in lexical order.
+ *
+ * @see <a href="https://leetcode.com/problems/reconstruct-itinerary/">332. Reconstruct Itinerary</a>
+ * User: liyulin
+ * Date: 2019/10/25
+ */
+public class ReconstructItinerary {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> flights = new HashMap<>();
+        LinkedList<String> path = new LinkedList<>();
+        for (List<String> ticket : tickets) {
+            flights.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            flights.get(ticket.get(0)).add(ticket.get(1));
+        }
+        dfs("JFK", flights, path);
+        return path;
+    }
+
+    private void dfs(String departure, Map<String, PriorityQueue<String>> flights, LinkedList<String> path) {
+        PriorityQueue<String> arrivals = flights.get(departure);
+        while (arrivals != null && !arrivals.isEmpty()) {
+            dfs(arrivals.poll(), flights, path);
+        }
+        path.addFirst(departure);
+    }
+}
