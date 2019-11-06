27de6a3f60f9454254d08ce23fb3501767fd2106
imshhui <imshhui@gmail.com>
2019-11-06 15:38:04 +0800

    DP solution for Combination sum IV

---
+++ src/main/java/com/gmail/imshhui/medium/CombinationSumIV.java
@@ -0,0 +1,62 @@
+package com.gmail.imshhui.medium;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+ *
+ * Example:
+ *
+ * nums = [1, 2, 3]
+ * target = 4
+ *
+ * The possible combination ways are:
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ *
+ * Note that different sequences are counted as different combinations.
+ *
+ * Therefore the output is 7.
+ *
+ *
+ * Follow up:
+ * What if negative numbers are allowed in the given array?
+ * How does it change the problem?
+ * What limitation we need to add to the question to allow negative numbers?
+ *
+ * Credits:
+ * Special thanks to @pbrother for adding this problem and creating all test cases.
+ *
+ * @see <a href="https://leetcode.com/problems/combination-sum-iv/">377. Combination Sum IV</a>
+ * User: liyulin
+ * Date: 2019/11/6
+ */
+public class CombinationSumIV {
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return backtrace(nums, target);
+    }
+
+    private int backtrace(int[] nums, int target) {
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res = res + backtrace(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
+    }
+}
