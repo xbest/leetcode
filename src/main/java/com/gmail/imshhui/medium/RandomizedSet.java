16c2acc34dff8a3f3c389e6a5f485fd9cc656850
imshhui <imshhui@gmail.com>
2019-11-06 16:59:26 +0800

    two hashmaps solution for Insert Delte GEtRandom O(1) in RandomizedSet

---
+++ src/main/java/com/gmail/imshhui/medium/RandomizedSet.java
@@ -0,0 +1,98 @@
+package com.gmail.imshhui.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Design a data structure that supports all following operations in average O(1) time.
+ *
+ * insert(val): Inserts an item val to the set if not already present.
+ * remove(val): Removes an item val from the set if present.
+ * getRandom: Returns a random element from current set of elements.
+ * Each element must have the same probability of being returned.
+ * Example:
+ *
+ * // Init an empty set.
+ * RandomizedSet randomSet = new RandomizedSet();
+ *
+ * // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+ * randomSet.insert(1);
+ *
+ * // Returns false as 2 does not exist in the set.
+ * randomSet.remove(2);
+ *
+ * // Inserts 2 to the set, returns true. Set now contains [1,2].
+ * randomSet.insert(2);
+ *
+ * // getRandom should return either 1 or 2 randomly.
+ * randomSet.getRandom();
+ *
+ * // Removes 1 from the set, returns true. Set now contains [2].
+ * randomSet.remove(1);
+ *
+ * // 2 was already in the set, so return false.
+ * randomSet.insert(2);
+ *
+ * // Since 2 is the only number in the set, getRandom always return 2.
+ * randomSet.getRandom();
+ *
+ * @see <a href="https://leetcode.com/problems/insert-delete-getrandom-o1/">380. Insert Delete GetRandom O(1)</a>
+ * User: liyulin
+ * Date: 2019/11/6
+ */
+public class RandomizedSet {
+    private Map<Integer, Integer> map;
+    private Map<Integer, Integer> indexMap;
+    private Random random;
+    private int index;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        indexMap = new HashMap<>();
+        random = new Random();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        } else {
+            indexMap.put(index, val);
+            map.put(val, index);
+            index++;
+            return true;
+        }
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (map.containsKey(val)) {
+            int index = map.get(val);
+            map.remove(val);
+            indexMap.remove(index);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        while (true) {
+            int i = random.nextInt(index);
+            if (indexMap.containsKey(i)) {
+                return indexMap.get(i);
+            }
+        }
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
