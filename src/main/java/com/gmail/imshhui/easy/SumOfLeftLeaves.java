f36659b3ef2ff9b7aca9663d900fbf42ff86e045
imshhui <imshhui@gmail.com>
2019-11-14 15:26:22 +0800

    Recursive solution for Sum of Left Leaves

---
+++ src/main/java/com/gmail/imshhui/easy/SumOfLeftLeaves.java
@@ -0,0 +1,38 @@
+package com.gmail.imshhui.easy;
+
+import com.gmail.imshhui.bean.TreeNode;
+
+/**
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * Example:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ *
+ * @see <a href="https://leetcode.com/problems/sum-of-left-leaves/">404. Sum of Left Leaves</a>
+ * User: liyulin
+ * Date: 2019/11/14
+ */
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                res = res + root.left.val;
+            } else {
+                res = res + sumOfLeftLeaves(root.left);
+            }
+        }
+        res = res + sumOfLeftLeaves(root.right);
+        return res;
+    }
+}
