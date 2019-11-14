5c1a920d89028e0ed15c6ffc92f29a270ec46a24
imshhui <imshhui@gmail.com>
2019-11-14 16:03:48 +0800

    fix corner case 0 for Convert a Number to Hexadecimal

--- src/main/java/com/gmail/imshhui/easy/ConvertANumberToHexadecimal.java
+++ src/main/java/com/gmail/imshhui/easy/ConvertANumberToHexadecimal.java
@@ -32,6 +32,10 @@ package com.gmail.imshhui.easy;
  */
 public class ConvertANumberToHexadecimal {
     public String toHex(int num) {
+        // corner case: 0
+        if (num == 0) {
+            return "0";
+        }
         char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         StringBuilder res = new StringBuilder();
         // base case: -1, if num > 0
