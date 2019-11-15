24196cc24a080de78aef030ad1be164d613b3f2b
imshhui <imshhui@gmail.com>
2019-11-15 10:55:13 +0800

    Arithmetic Slices

---
+++ src/main/java/com/gmail/imshhui/medium/ArithmeticSlices.java
@@ -0,0 +1,47 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * A sequence of number is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+ *
+ * For example, these are arithmetic sequence:
+ *
+ * 1, 3, 5, 7, 9
+ * 7, 7, 7, 7
+ * 3, -1, -5, -9
+ * The following sequence is not arithmetic.
+ *
+ * 1, 1, 2, 5, 7
+ *
+ * A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+ *
+ * A slice (P, Q) of array A is called arithmetic if the sequence:
+ * A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+ *
+ * The function should return the number of arithmetic slices in the array A.
+ *
+ *
+ * Example:
+ *
+ * A = [1, 2, 3, 4]
+ *
+ * return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+ *
+ * @see <a href="https://leetcode.com/problems/arithmetic-slices/">413. Arithmetic Slices</a>
+ * User: liyulin
+ * Date: 2019/11/15
+ */
+public class ArithmeticSlices {
+    public int numberOfArithmeticSlices(int[] A) {
+        int cur = 2;
+        int sum = 0;
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                cur++;
+                sum = sum + cur;
+            } else {
+                cur = 0;
+            }
+        }
+        return sum;
+    }
+}
