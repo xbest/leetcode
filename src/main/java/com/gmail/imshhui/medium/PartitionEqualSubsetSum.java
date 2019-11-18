d7cb5fffbcac0eb820ff45f70b85ebd100a4d52a
imshhui <imshhui@gmail.com>
2019-11-18 18:25:54 +0800

    DP solution for Partition Equal Subset Sum

---
+++ src/main/java/com/gmail/imshhui/medium/PartitionEqualSubsetSum.java
@@ -0,0 +1,56 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given a non-empty array containing only positive integers,
+ * ind if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+ *
+ * Note:
+ *
+ * Each of the array element will not exceed 100.
+ * The array size will not exceed 200.
+ *
+ *
+ * Example 1:
+ *
+ * Input: [1, 5, 11, 5]
+ *
+ * Output: true
+ *
+ * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ *
+ *
+ * Example 2:
+ *
+ * Input: [1, 2, 3, 5]
+ *
+ * Output: false
+ *
+ * Explanation: The array cannot be partitioned into equal sum subsets.
+ *
+ * @see <a href="https://leetcode.com/problems/partition-equal-subset-sum/">416. Partition Equal Subset Sum</a>
+ * User: liyulin
+ * Date: 2019/11/15
+ */
+public class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum = sum + num;
+        }
+        if ((sum & 1) != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+}
