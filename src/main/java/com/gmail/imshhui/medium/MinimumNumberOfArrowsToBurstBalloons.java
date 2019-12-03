89fab6a52089ec9f5bb2852ba4582934f268825e
imshhui <imshhui@gmail.com>
2019-12-03 18:13:20 +0800

    Greedy solution for Minimum Number of Arrows to Burst Balloons

--- src/main/java/com/gmail/imshhui/medium/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/main/java/com/gmail/imshhui/medium/MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,5 +1,6 @@
 package com.gmail.imshhui.medium;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -48,6 +49,23 @@ public class MinimumNumberOfArrowsToBurstBalloons {
                 end = point[1];
                 res++;
             }
+        }
+        return res;
+    }
+
+    public int findMinArrowShots1(int[][] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int res = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= end) {
+                continue;
+            }
+            res++;
+            end = points[i][1];
         }
         return res;
     }
