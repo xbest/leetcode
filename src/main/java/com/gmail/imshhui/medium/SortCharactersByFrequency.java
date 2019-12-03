e06a1e2f3774fcd553fef5a9bdc57459ac0a1409
imshhui <imshhui@gmail.com>
2019-12-03 17:44:22 +0800

    PriorityQueue solution for Sort Characters By Frequency

---
+++ src/main/java/com/gmail/imshhui/medium/SortCharactersByFrequency.java
@@ -0,0 +1,66 @@
+package com.gmail.imshhui.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * Given a string, sort it in decreasing order based on the frequency of characters.
+ *
+ * Example 1:
+ *
+ * Input:
+ * "tree"
+ *
+ * Output:
+ * "eert"
+ *
+ * Explanation:
+ * 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * Example 2:
+ *
+ * Input:
+ * "cccaaa"
+ *
+ * Output:
+ * "cccaaa"
+ *
+ * Explanation:
+ * Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+ * Note that "cacaca" is incorrect, as the same characters must be together.
+ * Example 3:
+ *
+ * Input:
+ * "Aabb"
+ *
+ * Output:
+ * "bbAa"
+ *
+ * Explanation:
+ * "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ * Note that 'A' and 'a' are treated as two different characters.
+ *
+ * @see <a href="https://leetcode.com/problems/sort-characters-by-frequency/">451. Sort Characters By Frequency</a>
+ * User: liyulin
+ * Date: 2019/12/3
+ */
+public class SortCharactersByFrequency {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        queue.addAll(map.entrySet());
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            Map.Entry<Character, Integer> entry = queue.poll();
+            for (int i = 0; i < entry.getValue(); i++) {
+                sb.append(entry.getKey());
+            }
+        }
+        return sb.toString();
+    }
+}
