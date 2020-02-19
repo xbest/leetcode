ec44199531f93b1ba47565adc933e56720e157da
imshhui <imshhui@gmail.com>
2020-02-19 11:20:16 +0800

    DP solution for Longest Palindromic Subsequence

---
+++ src/main/java/com/gmail/imshhui/medium/CoinChange2.java
@@ -0,0 +1,8 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * User: liyulin
+ * Date: 2020/2/19
+ */
+public class CoinChange2 {
+}
---
+++ src/main/java/com/gmail/imshhui/medium/LongestPalindromicSubsequence.java
@@ -0,0 +1,40 @@
+package com.gmail.imshhui.medium;
+
+/**
+ * Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example 1:
+ * Input:
+ *
+ * "bbbab"
+ * Output:
+ * 4
+ * One possible longest palindromic subsequence is "bbbb".
+ * Example 2:
+ * Input:
+ *
+ * "cbbd"
+ * Output:
+ * 2
+ * One possible longest palindromic subsequence is "bb".
+ *
+ * @see <a href="https://leetcode.com/problems/longest-palindromic-subsequence/">516. Longest Palindromic Subsequence</a>
+ * User: liyulin
+ * Date: 2020/2/19
+ */
+public class LongestPalindromicSubsequence {
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+}
