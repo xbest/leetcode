0d27f0b4b7f68e6c3a21ed8f280f48f2f0b9fed8
imshhui <imshhui@gmail.com>
2020-02-25 10:44:06 +0800

    Contiguous Array

--- src/main/java/com/gmail/imshhui/medium/ContiguousArray.java
+++ src/main/java/com/gmail/imshhui/medium/ContiguousArray.java
@@ -1,8 +1,45 @@
 package com.gmail.imshhui.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+ *
+ * Example 1:
+ * Input: [0,1]
+ * Output: 2
+ * Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+ *
+ * Example 2:
+ * Input: [0,1,0]
+ * Output: 2
+ * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ *
+ * Note: The length of the given binary array will not exceed 50,000.
+ *
+ * @see <a href="https://leetcode.com/problems/contiguous-array/">525. Contiguous Array</a>
  * User: liyulin
  * Date: 2020/2/24
  */
 public class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] == 0 ? -1 : nums[i];
+        }
+        int max = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (map.containsKey(sum)) {
+                max = Math.max(max, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return max;
+    }
 }
--- src/test/java/com/gmail/imshhui/medium/ContiguousArrayTest.java
+++ src/test/java/com/gmail/imshhui/medium/ContiguousArrayTest.java
@@ -1,8 +1,16 @@
 package com.gmail.imshhui.medium;
 
+import org.junit.Test;
+
 /**
  * User: liyulin
  * Date: 2020/2/24
  */
 public class ContiguousArrayTest {
+    ContiguousArray client = new ContiguousArray();
+
+    @Test
+    public void findMaxLength() {
+        client.findMaxLength(new int[]{1, 1, 0, 1});
+    }
 }
