fde434219bca330a75452d624b4008cd3d1ca6af
imshhui <imshhui@gmail.com>
2020-02-26 17:35:42 +0800

    O(lgN) solution for Single Element in a Sorted Array

--- src/main/java/com/gmail/imshhui/medium/SingleElementInASortedArray.java
+++ src/main/java/com/gmail/imshhui/medium/SingleElementInASortedArray.java
@@ -33,4 +33,21 @@ public class SingleElementInASortedArray {
         }
         return 0;
     }
+
+    public int singleNonDuplicatelogN(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 2;
+            }
+        }
+        return nums[low];
+    }
 }
